ef7358f202e895946f43383be953664e02f28d2a
Faizan Safdar Ali <faizan.ali@educative.io>
2021-11-04 00:17:12 -0400

    Netowrk layer final

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,4 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
-import tw from "tailwind-react-native-classnames";
 import { NavigationContainer } from "@react-navigation/native";
 import NavTabs from "../components/NavBar";
 
